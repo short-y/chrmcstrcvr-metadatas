141b64044d3ac3fb622a70417c96bf4474fcffe3
Tony <1022162+short-y@users.noreply.github.com>
2025-12-10 12:35:49 -0800

    feat: Enable verbose Cast SDK logging
    Enabled verbose logging for the Cast SDK in KoztApp.kt's onCreate method to provide more detailed debug information regarding Cast device discovery.

--- app/src/main/java/com/example/castkozt/KoztApp.kt
+++ app/src/main/java/com/example/castkozt/KoztApp.kt
@@ -1,9 +1,18 @@
 package com.example.castkozt
 
 import android.app.Application
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.CastMediaControlIntent
+import android.util.Log // Import Log for debugging
 
 class KoztApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        // Enable verbose logging for Cast SDK
+        CastContext.getSharedInstance(this).setLoggerLevel(Log.DEBUG)
+        Log.d("KoztApp", "Cast SDK verbose logging enabled.")
     }
 }
