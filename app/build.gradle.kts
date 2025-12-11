25775c96084c017ba362dd332351d92079067611
Tony <1022162+short-y@users.noreply.github.com>
2025-12-11 10:50:06 -0800

    Update targetSdk and compileSdk to 35 for Google Play requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.tonystakeontech.castkozt"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tonystakeontech.castkozt"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.1"
 
