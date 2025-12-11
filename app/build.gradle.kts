232b7fbe3cda54078fcbee233e23b62af4e1442d
Tony <1022162+short-y@users.noreply.github.com>
2025-12-10 16:00:55 -0800

    chore(android): add debug.keystore and config for seamless updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,10 +20,22 @@ android {
         }
     }
 
+    signingConfigs {
+        create("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
